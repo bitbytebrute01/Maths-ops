773b47b5aee2461ef570e735d9817980522d856c
bitbytebrute01 <bitbytebrute01@gmail.com>
2025-10-28 11:28:56 +0530

    initial commit

---
+++ studentDetails.py
@@ -0,0 +1,10 @@
+name = "Aditya Joshi"
+usn = "01fe24bca017"
+age = 19
+marks = 30
+sem = "3rd"
+print(f"The name : {name}")
+print(f"The name : {usn}")
+print(f"The name : {marks}")
+print(f"The name : {sem}")
+print(f"The name : {age}")
